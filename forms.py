53dbdab95fb3d6f8993933ec99be91cd14d16038
ericgarig <erik.shagdar@gmail.com>
2017-07-02 19:48:28 -0400

    updated contact form - text color, removed labels, thinner inputs

--- forms.py
+++ forms.py
@@ -1,15 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField as string, TextAreaField as text
 from wtforms.validators import DataRequired, Email
 
 
 class ContactForm(FlaskForm):
-    email = StringField('email',
-                        validators=[DataRequired(), Email()],
-                        render_kw={'placeholder': 'email'})
-    subject = StringField('subject',
-                          validators=[DataRequired()],
-                          render_kw={'placeholder': 'message'})
-    message = TextAreaField('message',
-                            validators=[DataRequired()],
-                            render_kw={'placeholder': 'message'})
+    email = string('email',
+                   validators=[DataRequired(message='Email is required'),
+                               Email(message='Enter a valid email')],
+                   render_kw={'placeholder': 'email'})
+    subject = string('subject',
+                     validators=[DataRequired(message='Enter a subject')],
+                     render_kw={'placeholder': 'subject'})
+    message = text('message',
+                   validators=[DataRequired(message='Send me a message')],
+                   render_kw={'placeholder': 'message'})
