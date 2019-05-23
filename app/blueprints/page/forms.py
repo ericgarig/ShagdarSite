4a9fbef7d0a4d517dbdc56217088d404872b5107
Erik Shagdar <erik.shagdar@gmail.com>
2019-05-23 16:41:08 -0400

    added recaptcha to contact form.

--- app/blueprints/page/forms.py
+++ app/blueprints/page/forms.py
@@ -1,5 +1,5 @@
 """Forms out page blueprint."""
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import TextAreaField
 from wtforms_components import EmailField
 from wtforms.validators import DataRequired, Length
@@ -8,5 +8,6 @@ from wtforms.validators import DataRequired, Length
 class ContactForm(FlaskForm):
     """Contact form."""
 
-    email = EmailField('email', [DataRequired(), Length(3, 254)])
-    message = TextAreaField('message', [DataRequired(), Length(1, 8192)])
+    email = EmailField("email", [DataRequired(), Length(3, 254)])
+    message = TextAreaField("message", [DataRequired(), Length(1, 8192)])
+    recaptcha = RecaptchaField()
--- app/blueprints/page/views.py
+++ app/blueprints/page/views.py
@@ -4,10 +4,10 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 from blueprints.page.forms import ContactForm
 
 
-page = Blueprint('page', __name__, template_folder='templates')
+page = Blueprint("page", __name__, template_folder="templates")
 
 
-@page.route('/', methods=['GET', 'POST'])
+@page.route("/", methods=["GET", "POST"])
 def index():
     """Render index."""
     form = ContactForm()
@@ -15,10 +15,11 @@ def index():
         # This prevents circular imports
         from blueprints.page.tasks import deliver_contact_email
 
-        deliver_contact_email(request.form.get('email'),
-                              request.form.get('message'))
-        flash('Thanks, expect a response shortly.', 'success')
-        return redirect(url_for('page.index'))
-    elif request.method == 'POST':
-        flash('Looks like you tried to submit an empty form.', 'warning')
-    return render_template('page/index.html', form=form)
+        deliver_contact_email(
+            request.form.get("email"), request.form.get("message")
+        )
+        flash("Thanks, expect a response shortly.", "success")
+        return redirect(url_for("page.index"))
+    elif request.method == "POST":
+        flash("Looks like you tried to submit an empty form.", "warning")
+    return render_template("page/index.html", form=form)
